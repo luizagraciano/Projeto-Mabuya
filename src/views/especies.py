d57383e21a6de11a36c18cd9d95eb10bd1039b01
Luiza Graciano <luizagraciano.m@gmail.com>
2024-09-16 00:23:42 -0300

    atualiza paginas

--- src/views/especies.py
+++ src/views/especies.py
@@ -4,6 +4,12 @@ from src.data.db import get_db
 #Cria a blueprint do módulo de avistamentos
 bp = Blueprint('especies', __name__, url_prefix='/especies')
 
-@bp.route('/')
-def especies():
-    return render_template('especies.html')
+@bp.route('/<nome>')
+def especie(nome):
+    db = get_db()
+    especie = db.execute(
+        'SELECT * FROM especime WHERE nome_popular = ? COLLATE NOCASE', (nome,)
+        ).fetchone()
+    
+    return render_template('especies.html', especie = especie)
+
